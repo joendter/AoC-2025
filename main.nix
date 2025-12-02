8908fd4b6f64984c26bb66b3452e41b511b502f9
joendter <71767955+joendter@users.noreply.github.com>
2025-12-02 10:21:02 +0100

    add helper functions

--- main.nix
+++ main.nix
@@ -1,3 +1,10 @@
-{
-
-}
+with builtins;
+with rec {
+    pkgs = import <nixpkgs> {};
+    mod = n: modulus: pkgs.lib.trivial.mod ((pkgs.lib.trivial.mod n modulus)+ modulus) modulus;
+    nicesplit = (splitOn: string: filter (x: isString x) (split splitOn string));
+    raw = readFile ./input.txt;
+    lines = nicesplit "\n" raw;
+    relevant_lines = filter (x: stringLength x > 0) lines;
+};
+lines
