0a8d6eb83c7a7e662b8c888ec913f50a30ca803c
joendter <71767955+joendter@users.noreply.github.com>
2025-12-03 16:33:17 +0100

    part1

--- main.nix
+++ main.nix
@@ -6,8 +6,22 @@ with rec {
     range = pkgs.lib.lists.range;
     sum = list: foldl' add 0 list;
     ceil = a: b: (a - 1) / b + 1;
+    as_char_array = string: filter (x: stringLength x > 0) (nicesplit "" string); 
+    reverse = pkgs.lib.lists.reverseList;
+    tail = pkgs.lib.lists.tail;
+    index_of = predicate: list: 
+    if predicate (head list) then 0 else (index_of predicate (tail list) + 1);
     raw = readFile ./input.txt;
     lines = nicesplit "\n" raw;
     relevant_lines = filter (x: stringLength x > 0) lines;
+    find_best_in_line = line:
+    with rec {
+        nums = map (x: fromJSON x) (as_char_array line);
+        n1 = foldl' (pkgs.lib.trivial.max) 0 (tail (reverse nums));
+        n2 = foldl' (pkgs.lib.trivial.max) 0 (
+            pkgs.lib.lists.drop (index_of (x: x == n1) nums + 1) nums
+        );
+    };
+    n1 * 10 + n2;
 };
-lines
+sum (map find_best_in_line relevant_lines)
