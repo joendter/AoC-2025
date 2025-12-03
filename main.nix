ff92eeb67b529c6a25459add03a1e469d6d50fbb
joendter <71767955+joendter@users.noreply.github.com>
2025-12-03 16:52:13 +0100

    part2

--- main.nix
+++ main.nix
@@ -11,17 +11,28 @@ with rec {
     tail = pkgs.lib.lists.tail;
     index_of = predicate: list: 
     if predicate (head list) then 0 else (index_of predicate (tail list) + 1);
+    max = list: foldl' pkgs.lib.trivial.max 0 list;
+    drop = pkgs.lib.lists.drop;
+    digits = n: if n < 10 then 1 else (digits (n / 10)) + 1;
+    power = base: exponent: if exponent == 0 then 1 else (power base (exponent - 1)) * base;
+
     raw = readFile ./input.txt;
     lines = nicesplit "\n" raw;
     relevant_lines = filter (x: stringLength x > 0) lines;
+    find_total = nums: to_go:
+    if to_go == 0 then 0 else
+    with rec {
+        n = max (drop (to_go - 1) (reverse nums));
+        rest = drop (index_of (x: x == n) nums + 1) nums;
+        rest_result = find_total rest (to_go - 1);
+        mult = power 10 (digits rest_result);
+    };
+    n * mult + rest_result;
+
     find_best_in_line = line:
     with rec {
         nums = map (x: fromJSON x) (as_char_array line);
-        n1 = foldl' (pkgs.lib.trivial.max) 0 (tail (reverse nums));
-        n2 = foldl' (pkgs.lib.trivial.max) 0 (
-            pkgs.lib.lists.drop (index_of (x: x == n1) nums + 1) nums
-        );
     };
-    n1 * 10 + n2;
+    (find_total nums 12) / 10;
 };
 sum (map find_best_in_line relevant_lines)
