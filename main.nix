f3da8c7569c699edccd4cce50a23d673be3a9035
joendter <71767955+joendter@users.noreply.github.com>
2025-12-03 16:04:19 +0100

    add sum and ceil

--- main.nix
+++ main.nix
@@ -3,6 +3,9 @@ with rec {
     pkgs = import <nixpkgs> {};
     mod = n: modulus: pkgs.lib.trivial.mod ((pkgs.lib.trivial.mod n modulus)+ modulus) modulus;
     nicesplit = (splitOn: string: filter (x: isString x) (split splitOn string));
+    range = pkgs.lib.lists.range;
+    sum = list: foldl' add 0 list;
+    ceil = a: b: (a - 1) / b + 1;
     raw = readFile ./input.txt;
     lines = nicesplit "\n" raw;
     relevant_lines = filter (x: stringLength x > 0) lines;
