10ecde881a0611d2f4eaea1a07b462124a4339d6
joendter <71767955+joendter@users.noreply.github.com>
2025-12-05 09:48:16 +0100

    part1

--- main.nix
+++ main.nix
@@ -1,8 +1,13 @@
 with builtins;
 with (import ./aoclib.nix);
 with rec {
-    raw = readFile ./example.txt;
-    lines = nicesplit "\n" raw;
-    relevant_lines = filter (x: stringLength x > 0) lines;
+    raw = readFile ./input.txt;
+    stuff = map (x: filter (x: x != "") (nicesplit "\n" x)) (nicesplit "\n\n" raw);
+    ranges = map (s: map fromJSON (nicesplit "-" s)) (elemAt stuff 0);
+    ids = map fromJSON (elemAt stuff 1);
+
+    in_range = id: range: 
+        elemAt range 0 <= id &&
+        elemAt range 1 >= id;
 };
-lines
+length (filter (id: any (l: in_range id l) ranges) ids)
