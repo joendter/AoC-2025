949755cef5e9c42ccf8493f7b951752a1f51227c
joendter <71767955+joendter@users.noreply.github.com>
2025-12-05 10:32:32 +0100

    part2

--- main.nix
+++ main.nix
@@ -9,5 +9,34 @@ with rec {
     in_range = id: range: 
         elemAt range 0 <= id &&
         elemAt range 1 >= id;
+
+    merge_range = range1: range2:
+        if elemAt range1 0 <= elemAt range2 1 && 
+           elemAt range1 1 >= elemAt range2 0
+        then
+            [[(pkgs.lib.trivial.min (elemAt range1 0) (elemAt range2 0))
+             (pkgs.lib.trivial.max (elemAt range1 1) (elemAt range2 1))
+            ]]
+            else
+            [range1 range2];
+    rangesort = ranges: pkgs.lib.lists.sort (a: b: elemAt a 1 > elemAt b 1) ranges;
+    merge_with_ranges = ranges: range:
+        if length ranges == 0 then [range] else
+        with rec {
+            sorted = rangesort ranges;
+            merged = merge_range range (head sorted);
+            rest = tail sorted;
+        };
+        if length merged == 2 then (rest ++ merged) else
+        merge_with_ranges rest (elemAt merged 0);
+
+
 };
-length (filter (id: any (l: in_range id l) ranges) ids)
+sum (
+map (range: elemAt range 1 - elemAt range 0 + 1) 
+(foldl' (merged: range: 
+    merge_with_ranges merged range
+) [] 
+(reverse (rangesort ranges))
+)
+)
